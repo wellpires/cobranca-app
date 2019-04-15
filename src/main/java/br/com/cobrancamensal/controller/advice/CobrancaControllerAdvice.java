e68c7a8c974e192bea6f1c51f415d8305b6a1ec6
wops <wops@BRPC008913.gft.com>
2019-04-14 22:47:14 -0300

    Adicionando logs

--- src/main/java/br/com/cobrancamensal/controller/advice/CobrancaControllerAdvice.java
+++ src/main/java/br/com/cobrancamensal/controller/advice/CobrancaControllerAdvice.java
@@ -1,5 +1,7 @@
 package br.com.cobrancamensal.controller.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,14 +18,19 @@ import br.com.cobrancamensal.response.ErrorResponse;
 @RestControllerAdvice
 public class CobrancaControllerAdvice {
 
+	Logger logger = LoggerFactory.getLogger(CobrancaControllerAdvice.class);
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+		logger.error(exception.getMessage(), exception);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(exception.getMessage()));
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
 			MethodArgumentNotValidException methodArgumentNotValidException) {
+		logger.error(methodArgumentNotValidException.getBindingResult().getFieldError().getDefaultMessage(),
+				methodArgumentNotValidException);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(
 				methodArgumentNotValidException.getBindingResult().getFieldError().getDefaultMessage()));
 	}
@@ -31,24 +38,28 @@ public class CobrancaControllerAdvice {
 	@ExceptionHandler(ClienteNotFoundException.class)
 	public ResponseEntity<ErrorResponse> handleClienteNotFoundException(
 			ClienteNotFoundException clienteNotFoundException) {
+		logger.error(clienteNotFoundException.getMessage(), clienteNotFoundException);
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(new ErrorResponse(clienteNotFoundException.getMessage()));
 	}
 
 	@ExceptionHandler(PlanoNotFoundException.class)
 	public ResponseEntity<ErrorResponse> handlePlanoNotFoundException(PlanoNotFoundException planoNotFoundException) {
+		logger.error(planoNotFoundException.getMessage(), planoNotFoundException);
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(planoNotFoundException.getMessage()));
 	}
 
 	@ExceptionHandler(PlanoDuplicadoException.class)
 	public ResponseEntity<ErrorResponse> handlePlanoDuplicadoException(
 			PlanoDuplicadoException planoDuplicadoException) {
+		logger.error(planoDuplicadoException.getMessage(), planoDuplicadoException);
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(planoDuplicadoException.getMessage()));
 	}
 
 	@ExceptionHandler(ClienteDuplicadoException.class)
 	public ResponseEntity<ErrorResponse> handleClienteDuplicadoException(
 			ClienteDuplicadoException clienteDuplicadoException) {
+		logger.error(clienteDuplicadoException.getMessage(), clienteDuplicadoException);
 		return ResponseEntity.status(HttpStatus.CONFLICT)
 				.body(new ErrorResponse(clienteDuplicadoException.getMessage()));
 	}
@@ -56,6 +67,7 @@ public class CobrancaControllerAdvice {
 	@ExceptionHandler(ContratoDuplicadoException.class)
 	public ResponseEntity<ErrorResponse> handleContratoDuplicadoException(
 			ContratoDuplicadoException contratoDuplicadoException) {
+		logger.error(contratoDuplicadoException.getMessage(), contratoDuplicadoException);
 		return ResponseEntity.status(HttpStatus.CONFLICT)
 				.body(new ErrorResponse(contratoDuplicadoException.getMessage()));
 	}
